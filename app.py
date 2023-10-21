641b227744b699718b172d91ce5127b8f9be8b83
Yunki <kmj9ab@virginia.edu>
2023-10-21 17:20:38 -0400

    app.py changed to accomodate the model

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import pickle
+from salaryEstimator import full_pipeline
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
@@ -10,14 +12,37 @@ def hello_world():
 @app.route("/predict", methods=['POST'])
 def predict():
     age = int(request.form['Age'])
-    education = int(request.form['education'])
-    job_title = int(request.form['Job title'])
-    years = int(request.form['Year of Experience'])
-    prediction = model.predict([[age, education, job_title, years]]) 
-    model output [women, men's salary]
-    women = 1000
-    men = 1000
-    return render_template('index.html', prediction_text="The expected salaries for women is ${women}, and for men: ${men}")
+    education = str(request.form['Education'])
+    job_title = str(request.form['JobTitle'])
+    years = int(request.form['YearsOfExperience'])
+    user_input_data_male = {
+        'Age':[age],
+        'Gender':["Male"],
+        'Education Level':[education],
+        'Job Title':[job_title],
+        'Years of Experience':[years]
+    }
+    user_input_data_female = {
+        'Age':[age],
+        'Gender':["Female"],
+        'Education Level':[education],
+        'Job Title':[job_title],
+        'Years of Experience':[years]
+    }
+
+    user_input_prepared_male = full_pipeline.transform(user_input_data_male)
+    user_input_prepared_female = full_pipeline.transform(user_input_data_female)
+
+    predicted_salary_male = model.predict(user_input_prepared_male)
+    predicted_salary_female = model.predict(user_input_prepared_female)
+
+
+
+    #prediction = model.predict([[age, education, job_title, years]]) 
+    #model output [women, men's salary]
+    # women = 1000
+    # men = 1000
+    return render_template('index.html', prediction_text="The expected salaries for women is ${predicted_salary_female}, and for men: ${predicted_salary_male}")
 
 if __name__ == " __main__":
     app.run()
