15f2b489bca2262a8513b3bff6c83a809a0b61d5
Yunki <kmj9ab@virginia.edu>
2023-10-21 18:45:28 -0400

    obtained two graphs to visualize pay gap

--- salaryEstimator.py
+++ salaryEstimator.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import cross_val_score
 import pickle
+import seaborn as sns
 
 np.random.seed(42)
 
@@ -77,5 +78,18 @@ std_rmse = rmse_scores.std()
 # print(f"Mean RMSE: {mean_rmse}")
 # print(f"Standard Deviation of RMSE: {std_rmse}")
 #print(X_train.iloc[0])
+plot_data = pd.concat([X_train[['Job Title', 'Gender']], pd.Series(y_train, name='Salary')], axis=1)
+#pickle.dump(forest_reg, open('model.pkl','wb'))
 
-pickle.dump(forest_reg, open('model.pkl','wb'))
+custom_palette = {'Male': '#86a0fc', 'Female': 'pink'}
+
+# sns.barplot(data=plot_data, x='Salary', y='Job Title', hue='Gender',palette= custom_palette)
+# plt.figure(figsize=(14, 8))
+# plt.title("Comparison of Salaries for All Job Titles")
+# plt.show()
+
+plot_data = pd.concat([X_train[['Education Level', 'Gender']], pd.Series(y_train, name='Salary')], axis=1)
+sns.barplot(data=plot_data, x = 'Salary', y = 'Education Level', hue = 'Gender',palette=custom_palette)
+plt.figure(figsize=(14, 8))
+plt.title("Comparison of Salaries for All Education Level")
+plt.show()
